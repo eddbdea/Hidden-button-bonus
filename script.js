f45777f2d0d212e294f80e169507fc15572b2660
Eduard Badea <eduardbadea@outlook.com>
2025-01-15 16:13:01 +0200

    Update

---
+++ script.js
@@ -0,0 +1,35 @@
+function randomNumberInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ 
+function generateButtons() {
+    const buttonValue = document.getElementById('buttonCount').value
+    const container = document.getElementById('buttonsContainer')
+    container.innerHTML = ''
+    const winnerNumber = randomNumberInt(1, buttonValue)
+    console.log(winnerNumber)
+
+    if (buttonValue >= 1) {
+        for(let i = 1; i <= buttonValue; ++i) {
+            let button = document.createElement('button')
+            button.innerText = 'Button number ' + i
+            button.classList.add('game-button')
+            if (i == winnerNumber) {
+                button.addEventListener('click', showWinnerMessage)
+            } else {
+                button.addEventListener('click', showLoserMessage)
+            }
+            container.appendChild(button)
+        }
+    } else {
+        alert('Please enter a valid number greater than 0.')
+    }
+}
+
+function showWinnerMessage() {
+    return alert('You won!')
+}
+
+function showLoserMessage() {
+    return alert('You lost!')
+}
